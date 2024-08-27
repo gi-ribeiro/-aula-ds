5b2b5ebcf1b15c94326f7926a9929019ef7759bb
gi-ribeiro <00001132713614sp@al.educacao.sp.gov.br>
2024-08-27 20:28:55 -0300

    api.py

---
+++ api.py
@@ -0,0 +1,7 @@
+import requests 
+
+response = requests.get('https://servicodados.ibge.gov.br/api/v1/localidades/distritos')
+
+distritos = response.json()
+for distrito in distritos: 
+    print(distritos['nome']) 
