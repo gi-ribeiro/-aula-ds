2379ec998bb583f295a993caa7c0f0e3de3fd29f
gi-ribeiro <00001132713614sp@al.educacao.sp.gov.br>
2024-09-10 17:00:35 -0300

    atividadedistanciacarro.py

---
+++ atividadedistanciacarro.py
@@ -0,0 +1,5 @@
+distancia = float(input("Digite a distancia em km: "))
+velocidade_media = float(input("Digite a velocidade média esperada: "))
+
+tempo_viagem = distancia / velocidade_media 
+print(f"O tempo estimado da viagem é  de {tempo_viagem:.2f}horas.")
